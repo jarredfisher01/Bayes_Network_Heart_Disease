c2e1ab867975c8fc2c9b8ef134fb167cbf655158
shai aarons <shai@shais-MacBook-Pro-2.local>
2022-10-03 15:00:54 +0200

    CPT for heart disease Node

---
+++ src/heart_disease_table.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import csv 
+from tqdm import tqdm
+import numpy as np
+
+ # row_tuple = (heart_disease, high_blood_pressure, high_cholesterol, smoker, heavy_alcohol, physical_actvity)
+df = pd.DataFrame(columns=['heart_disease','high_blood_pressure','high_cholesterol','smoker','heavy_alcohol','physical_activity'])
+
+#Converting Raw data to pandas CSV
+# with open('./data/health.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+
+#     for row in tqdm(csv_reader):
+#         heart_disease = row[0]
+        
+#         if heart_disease=='1.0':
+#             heart_disease = True
+            
+#         else:
+#             heart_disease = False
+        
+#         high_blood_pressure = row[1]
+
+#         if high_blood_pressure =='1.0':
+#             high_blood_pressure = True
+#         else:
+#             high_blood_pressure = False
+        
+#         high_cholesterol = row[2]
+#         if high_cholesterol =='1.0':
+#             high_cholesterol = True
+#         else:
+#             high_cholesterol = False
+        
+#         smoker = row[5]
+#         if smoker == '1.0':
+#             smoker = True
+#         else:
+#             smoker = False
+
+#         physical_actvity = row[8]
+#         if physical_actvity == '1.0':
+#             physical_actvity = True
+#         else:
+#             physical_actvity = False
+
+#         heavy_alcohol = row[11]
+#         if heavy_alcohol == '1.0':
+#             heavy_alcohol = True
+#         else:
+#             heavy_alcohol = False
+    
+#         row_array = [heart_disease, high_blood_pressure, high_cholesterol, smoker, heavy_alcohol, physical_actvity]
+#         df.loc[len(df.index)] = row_array
+
+
+
+df = pd.read_csv('./data/pandas_df.csv')
+
+#Numerator
+combination_dict = {}
+for index, row in df.iterrows():
+    row_tuple = (row['heart_disease'], row['high_blood_pressure'],row['high_cholesterol'],row['smoker'], row['heavy_alcohol'], row['physical_activity'])
+    if row_tuple in combination_dict.keys():
+        combination_dict[row_tuple] = combination_dict[row_tuple]+1
+    else:
+        combination_dict[row_tuple] = 1
+
+total_rows = 0 
+for x in combination_dict.values():
+    total_rows+=x
+
+
+for key, value in combination_dict.items():
+    combination_dict[key] = value/total_rows
+
+numerator_df  = pd.DataFrame(columns=['heart_disease','high_blood_pressure','high_cholesterol','smoker','heavy_alcohol','physical_activity', 'probability'])
+
+
+#Denominator - evidence
+evidence_dict = {}
+for index, row in df.iterrows():
+    row_tuple = (row['high_blood_pressure'],row['high_cholesterol'],row['smoker'], row['heavy_alcohol'], row['physical_activity'])
+    if row_tuple in evidence_dict.keys():
+        evidence_dict[row_tuple] = evidence_dict[row_tuple]+1
+    else:
+        evidence_dict[row_tuple] = 1
+
+total_rows = 0 
+for x in evidence_dict.values():
+    total_rows+=x
+
+
+sum = 0 
+for key, value in evidence_dict.items():
+    evidence_dict[key] = value/total_rows
+    sum+=value/total_rows
+
+#Calculating conditional distribution
+conditional_probability_table = {}
+for key, value in combination_dict.items():
+    numerator_key = key 
+    numerator_value = value 
+
+    denominator_key = (numerator_key[1], numerator_key[2], numerator_key[3], numerator_key[4], numerator_key[5])
+    denominator_value = evidence_dict[denominator_key]
+
+    conditional_probability_table[key] = numerator_value/denominator_value
+
