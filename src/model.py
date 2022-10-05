7e0c9ea047705d90d15270b055af81089fa70d26
Joshua Rosenthal <rsnjos005@gmail.com>
2022-10-05 17:53:31 +0200

    Finished model

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ nodes=['Smokes', 'Hypertension', 'Heart_Disease', 'Cholesterol', 'Alcohol_Abuse'
 
 links = [('Smokes','Hypertension'),('Hypertension','Heart_Disease'),('Smokes','Heart_Disease'),
 ('Cholesterol','Heart_Disease'),('Alcohol_Abuse','Hypertension'),('Alcohol_Abuse','Heart_Disease'),('Physical_Activity','Heart_Disease'),
-('Heart_Disease','MI'),('MI','ST_Segment_Elevation'),('MI','Chest_Pain')]
+('Heart_Disease','MI'),('MI','ST_Segment_Elevation'),('Chest_Pain','MI')]
 
 
 def build_model():
@@ -45,10 +45,10 @@ def build_model_decision():
     
     dn.addUtilityNode(gum.LabelizedVariable("Utility","Utility",1))
    
-    links+=[("Call_Ambulance", "Utility"), ("Chest_Pain", "Utility")]
+    links+=[("Call_Ambulance", "Utility"),("MI","Utility")]
     
     for link in links:
-        #dn.addArc(dn.idFromName(link[0]),dn.idFromName(link[1]))
+        
         dn.addArc(*link)
     
     dn = addProbabilities(dn)
@@ -65,30 +65,15 @@ def build_model_decision():
     # 1 0 1
     # 1 1 0
     # 1 1 1
-    dn.utility('Utility')[{'Call_Ambulance':0, 'Chest_Pain':0}]=50    #Neutral
 
-    dn.utility('Utility')[{'Call_Ambulance':0, 'Chest_Pain':1}]= 25
+    dn.utility('Utility')[{'Call_Ambulance':0,'MI':0}]= 0   #You fine and nothing bad happens + no wasted medical resources
 
-    dn.utility('Utility')[{'Call_Ambulance':1, 'Chest_Pain':0}]= -25 #e.g. calling an ambulance for toothache/heartburn
+    #-150 working best 
+    dn.utility('Utility')[{'Call_Ambulance':0,'MI':1}]= -100   #You dead af
 
-    dn.utility('Utility')[{'Call_Ambulance':1,'Chest_Pain':1}]= 100
+    dn.utility('Utility')[{'Call_Ambulance':1,'MI':0}]= -50     #Wasted medical resources (and your own money)
 
-    # dn.utility('Utility')[{'Call_Ambulance':0,'MI':0, 'Chest_Pain':0}]=0    #Neutral
-
-    # dn.utility('Utility')[{'Call_Ambulance':0,'MI':0, 'Chest_Pain':1}] = 100
-
-    # dn.utility('Utility')[{'Call_Ambulance':0,'MI':1, 'Chest_Pain':0}]= -50  #e.g. calling an ambulance for toothache/heartburn
-
-    # dn.utility('Utility')[{'Call_Ambulance':0,'MI':1,'Chest_Pain':1}]= -100
-
-
-    # dn.utility('Utility')[{'Call_Ambulance':1,'MI':0, 'Chest_Pain':0}]= -100 
-
-    # dn.utility('Utility')[{'Call_Ambulance':1,'MI':0, 'Chest_Pain':1}]= 30
-
-    # dn.utility('Utility')[{'Call_Ambulance':1,'MI':1, 'Chest_Pain':0}]= 100
-
-    # dn.utility('Utility')[{'Call_Ambulance':1,'MI':1,'Chest_Pain':1}]= 150
+    dn.utility('Utility')[{'Call_Ambulance':1,'MI':1}]= 100     #Your life will most likely be saved
 
     dn.saveBIFXML("./output/MI_decision.bifxml")
     with open("./output/MI_decision.bifxml","r") as out:
@@ -123,15 +108,16 @@ def addProbabilities(model):
     #P(Cholestrol):
     model.cpt('Cholesterol').fillWith([0.59,0.41])
 
-
+    #P(Chest_Pain):
+    model.cpt('Chest_Pain').fillWith([0.7,0.3])
     #CPTs
     ##########
 
     #P(Hypertension | Smoking, Alcohol_Abuse):
 
     model.cpt("Hypertension")[{'Smokes':0, 'Alcohol_Abuse':0}] = [0.701, 0.299]
-    model.cpt("Hypertension")[{'Smokes':0, 'Alcohol_Abuse':1}] = [0.665, 0.335]
     model.cpt("Hypertension")[{'Smokes':1, 'Alcohol_Abuse':0}] = [0.715, 0.285]
+    model.cpt("Hypertension")[{'Smokes':0, 'Alcohol_Abuse':1}] = [0.665, 0.335]
     model.cpt("Hypertension")[{'Smokes':1, 'Alcohol_Abuse':1}] = [0.68, 0.32]
 
     #P(Heart_Disease | Hypertension, Cholesterol, Smokes, Alcohol_Abuse, Physical_Activity)
@@ -143,23 +129,25 @@ def addProbabilities(model):
 
     #P(MI | Heart_Disease):
 
-    model.cpt('MI')[{'Heart_Disease':0}] = [0.999609,1-0.999609]
-    model.cpt('MI')[{'Heart_Disease':1}] = [0.5489,0.4511]
+    # model.cpt('MI')[{'Heart_Disease':0}] = [0.999609,1-0.999609]
+    # model.cpt('MI')[{'Heart_Disease':1}] = [0.5489,0.4511]
 
     #P(ST_Segment_Elevation | MI):
 
-    model.cpt("ST_Segment_Elevation")[{'MI':0}] = [0.49,0.51]
-    model.cpt("ST_Segment_Elevation")[{'MI':1}] = [0.375,0.625]
+    model.cpt("ST_Segment_Elevation")[{'MI':0}] = [0.89,0.11]
+    model.cpt("ST_Segment_Elevation")[{'MI':1}] = [0.175,0.825]
+
+    
 
     # P(Chest Pain | MI):
 
-    model.cpt('Chest_Pain')[{'MI':0}] = [0.75,0.25]
-    model.cpt('Chest_Pain')[{'MI':1}] = [1/3,2/3]
+    # model.cpt('Chest_Pain')[{'MI':0}] = [0.75,0.25]
+    # model.cpt('Chest_Pain')[{'MI':1}] = [1/3,2/3]
 
     #P(MI | Heart_Disease, Chest_Pain):
 
 #     -|-- : 99,96% (1)
-# +|-- : 0,391%
+# +|-- : 0,4%
 
 # -|+- 54,89% (1)
 # +|+- 45,11%
@@ -170,8 +158,10 @@ def addProbabilities(model):
 # -|++ : 0.28
 # +|++ : 0.72 (2)
 
-
-
+    model.cpt("MI")[{'Heart_Disease':0, 'Chest_Pain':0}] = [0.9996, 0.0004]
+    model.cpt("MI")[{'Heart_Disease':0, 'Chest_Pain':1}] = [0.85,0.15]
+    model.cpt("MI")[{'Heart_Disease':1, 'Chest_Pain':0}] = [0.8489,0.1511]
+    model.cpt("MI")[{'Heart_Disease':1, 'Chest_Pain':1}] = [0.28,0.72]
 
     return model
 
@@ -181,6 +171,9 @@ def inference_decision(model, evidence = None):
     if ((evidence is not None) and (isinstance(evidence,dict))):
         ie.setEvidence(evidence)
     ie.makeInference()
+    for node in nodes:
+        print (ie.posterior(node))
+    print("Optimal Decision:")
     print(ie.optimalDecision("Call_Ambulance"))
 
 def inference(model, evidence = None):
--- src/wrapper.py
+++ src/wrapper.py
@@ -3,7 +3,11 @@ import model
 def main():
     bn = model.build_model_decision()
     
-    model.inference_decision(bn,{'Heart_Disease':0})
+    model.inference_decision(bn)
+
+    # bn = model.build_model()
+    
+    # model.inference(bn)
 
 if __name__ == "__main__":
     main()
